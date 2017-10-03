ca24f4f14130261f9528e2ffe0fafda96dca56b8
Yuriy Kashnikov <yuriy.kashnikov@gmail.com>
2017-10-03 11:40:40 +0700

    Update process dataset script

--- MacawTests/scripts/process-dataset.sh
+++ MacawTests/scripts/process-dataset.sh
@@ -2,9 +2,9 @@
 
 source settings.sh
 
-for i in $(ls ${ORIGINAL_DATASET}); do 
-	DST=$(basename $i)
-	echo $i
-	./trim-svg.py ${ORIGINAL_DATASET}/$i >${PROCESSED_DATASET}/$DST
-	./svg2png.sh "${PROCESSED_DATASET}/$DST"  "${REFERENCE_PNG}/$DST"
-done
+#for i in $(ls ${ORIGINAL_DATASET}); do 
+while read i; do
+	DST="${i}.svg"
+	./trim-svg.py ${ORIGINAL_DATASET}/$DST >${PROCESSED_DATASET}/$DST
+	./svg2png.sh "${PROCESSED_DATASET}/$DST"  "${REFERENCE_PNG}/${i}.png"
+done < all
