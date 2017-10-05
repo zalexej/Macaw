68bd2e5bbe5d023cf9e5d3f2c1c0372b1b70b048
Yuriy Kashnikov <yuriy.kashnikov@gmail.com>
2017-10-05 10:26:04 +0700

    Add fuzz 5% for color and switch to b&w colors diff

--- MacawTests/scripts/compare.sh
+++ MacawTests/scripts/compare.sh
@@ -5,7 +5,8 @@ SRCB=$2
 DST=$3
 
 rm -f result
-magick compare -metric RMSE $SRCA $SRCB -highlight-color  PaleGreen  -lowlight-color White $DST >result 2>&1 3>&1
+#magick compare -fuzz 5% -metric RMSE $SRCA $SRCB -highlight-color  PaleGreen  -lowlight-color White $DST >result 2>&1 3>&1
+magick compare -fuzz 5% -metric RMSE $SRCA $SRCB -compose src -highlight-color White -lowlight-color Black $DST >result 2>&1 3>&1 
 ae=`cat result`
 rm -f result
 
